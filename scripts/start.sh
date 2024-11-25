bbc275ee79a4edab565a000309b94fa240f39d72
NotACat1 <notacat.mail@gmail.com>
2024-11-25 17:30:39 +0300

    fix: исправление команды docker

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,5 @@ set -e
 source ../.env.deploy
 
 echo "Запуск проекта на сервере $SERVER_IP..."
-ssh "$USER@$SERVER_IP" "cd $DEPLOY_PATH && docker-compose up -d"
+ssh "$USER@$SERVER_IP" "cd $DEPLOY_PATH && docker compose up -d"
 echo "Проект запущен!"
